a571bfc78b7e47f1d13b5cb330d8f569332a2e28
German <dmgerman102@gmail.com>
2020-05-12 07:31:22 +0300

    puzzles

--- before_1000/337A_puzzles.py
+++ before_1000/337A_puzzles.py
@@ -1,14 +1,6 @@
 n, m = map(int, input().split())
-puzzles = list(map(int, input().split()))
-average = sum(puzzles)/m
-min_diff = min_id = 0
-buff = []
-while n:
-    min_diff = 0
-    for i in range(len(puzzles)):
-        if abs(puzzles[i]-average) < min_diff or not min_diff:
-            min_diff = abs(puzzles[i]-average)
-            min_id = i
-    buff.append(puzzles.pop(min_id))
-    n = n-1
-print(max(buff)-min(buff))
+puzzles = sorted(map(int, input().split()))
+best = float('inf')
+for k in range(m - n + 1):
+    best = min(best, puzzles[k + n - 1] - puzzles[k])
+print(best)
