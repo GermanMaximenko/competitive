3273d0bddd7cb298cf1246f95a738f4c5ae273b3
German <dmgerman102@gmail.com>
2020-05-14 22:13:49 +0300

    AB

--- after_1500/550A_two_substrings.py
+++ after_1500/550A_two_substrings.py
@@ -10,3 +10,7 @@ for i in range(1, len(s)):
     if cond:
         break
 print(['NO', 'YES'][cond])
+
+# s=input()
+# u=s.find('AB');v=s.find('BA')
+# print('YES'if (u+1 and s.find('BA',u+2)+1) or (v+1 and s.find('AB',v+2)+1)else'NO')
