a47387a63aaf0d97cf26930ace193734facc0fb1
German <dmgerman102@gmail.com>
2020-05-24 16:46:21 +0300

    Cartesian

--- my_set_of_problems/cartesian.py
+++ my_set_of_problems/cartesian.py
@@ -63,3 +63,18 @@ phrases = {
 # ]
 #
 # print(*itertools.product(*somelists))
+
+# --------------- third ---------------
+
+# test_list = [1, 4, 6, 7]
+# test_tup = (1, 3)
+# # printing original list and tuple
+# print("The original list : " + str(test_list))
+# print("The original tuple : " + str(test_tup))
+#
+# # Construct Cartesian Product Tuple list
+# # using list comprehension
+# res = [(a, b) for a in test_tup for b in test_list]
+#
+# # printing result
+# print("The Cartesian Product is : " + str(res))
