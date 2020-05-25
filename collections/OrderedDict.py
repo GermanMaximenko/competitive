0640992698340a9bf2269712138dc85df79b5b35
German <dmgerman102@gmail.com>
2020-05-25 22:29:00 +0300

    dict

--- collections/OrderedDict.py
+++ collections/OrderedDict.py
@@ -3,18 +3,38 @@ from collections import OrderedDict, Counter
 # 5. Write a Python program that accept some words and count the number of distinct words.
 # Print the number of distinct words and number of occurrences for each distinct word according to their appearance
 
+#
+# class OrderedCounter(Counter, OrderedDict):
+#     pass
+#
+#
+# word_array = []
+# n = int(input('input number of words, then the words\n'))
+# for i in range(n):
+#     word_array.append(input().strip())
+# word_ctr = OrderedCounter(word_array)
+# print(len(word_ctr))
+# for word in word_ctr:
+#     print(word_ctr[word], end=' ')
 
-class OrderedCounter(Counter, OrderedDict):
-    pass
+# 6. Write a Python program that accept name of given subject and marks.
+# Input number of subjects in first line and subject name,marks separated by a space in next line.
+# Print subject name and marks in order of its first occurrence
 
+# debug this
+import collections
+import  re
 
-word_array = []
-n = int(input('input number of words, then the words\n'))
+n = int(input("Number of subjects: "))
+item_order = collections.OrderedDict()
 for i in range(n):
-    word_array.append(input().strip())
-word_ctr = OrderedCounter(word_array)
-print(len(word_ctr))
-for word in word_ctr:
-    print(word_ctr[word], end=' ')
+    sub_marks_list = re.split(r'(\d+)$', input("Input Subject name and marks: ").strip())
+    subject_name = sub_marks_list[0]
+    item_price = int(sub_marks_list[1])
+    if subject_name not in item_order:
+        item_order[subject_name] = item_price
+    else:
+        item_order[subject_name] = item_order[subject_name] + item_price
 
-# разобратьс в коде и ordered dict
+for i in item_order:
+    print(i + str(item_order[i]))
