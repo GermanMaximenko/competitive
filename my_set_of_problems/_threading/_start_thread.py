941fc4432d879dcb53810bbfd04ef49970f9c962
maksimenko_gd <maksimenko_gd@magnit.ru>
2020-08-16 14:19:09 +0300

    start thread

---
+++ my_set_of_problems/_threading/_start_thread.py
@@ -0,0 +1,25 @@
+import threading
+import time
+
+
+def handler(started=0, finished=0):
+    print(f'INSIDE HANDLER  {threading.get_ident()}')
+    sum = 0
+    for i in range(started, finished):
+        sum += i
+    print('Result:', sum)
+
+
+params = {'finished': 2 ** 26}
+t1 = threading.Thread(target=handler, kwargs=params)
+
+print('Started by thread')
+start = time.time()
+t1.start()
+t1.join()
+print(f'Execute time = {time.time() - start}')
+
+start = time.time()
+print('Started by Main thread')
+handler(**params)
+print(f'Execute time = {time.time() - start}')
